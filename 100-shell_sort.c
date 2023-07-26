bfc899b7fee7cb8f712f2237d94172373f4876c7
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-07-26 05:37:37 +0300

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,41 +1,43 @@
 #include "sort.h"
 
-/**
-*swap - the positions of two elements into an array
-*@array: array
-*@item1: array element
-*@item2: array element
-*/
-void swap(int *array, int item1, int item2)
-{
 
-	int tmp;
-
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
-}
 /**
- * shell_sort - function that sorts an array of integers in ascending
- * order using the Shell sort algorithm, using the Knuth sequence
- * @size: size of the array
- * @array: list with numbers
+ * shell_sort - Sorts an array of integers in ascending order
+ *              using the Shell sort algorithm with the Knuth sequence.
+ * @array: The array to be sorted.
+ * @size: Size of the array.
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, i, index = 0;
+	size_t gap = 1, i, j;
 
 	if (array == NULL || size < 2)
 		return;
+
+	/* Calculate the initial gap using the Knuth sequence */
 	while (gap < size / 3)
 		gap = 3 * gap + 1;
+
+	/* Start Shell sort with decreasing gaps */
 	while (gap >= 1)
 	{
+		/* Perform insertion sort with current gap */
 		for (i = gap; i < size; i++)
-			for (index = i; index >= gap &&
-			 (array[index] < array[index - gap]); index -= gap)
-				swap(array, index, index - gap);
-		print_array(array, size);
+		{
+			int current = array[i];
+
+			/* Shift elements that are 'gap' positions apart */
+			for (j = i; j >= gap && current < array[j - gap]; j -= gap)
+				array[j] = array[j - gap];
+
+			/* Place the current element at its correct position */
+			array[j] = current;
+		}
+
+		/* Reduce the gap for the next iteration */
 		gap /= 3;
+
+		/* Print the current state of the array */
+		print_array(array, size);
 	}
 }
